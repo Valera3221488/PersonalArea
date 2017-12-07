7cb95caf6e11037d12b72f02b6a9d2140def96a2
Valera3221488 <magorik5@gmail.com>
2017-12-07 18:19:07 +0200

    First Commit

---
+++ app/Http/Controllers/MainPageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Routing\Controller as BaseController;
+
+
+class MainPageController extends BaseController
+{
+public function index(){
+
+    return ('welcome');
+}
+}
